e701ed9ccd810a49a7ca72ef64b10de38995dd9b
小毛同学 <741947159@qq.com>
2022-01-19 12:10:06 +0800

    bugfix

--- src/main/java/cn/itcast/sailing/service/VerificationService.java
+++ src/main/java/cn/itcast/sailing/service/VerificationService.java
@@ -15,37 +15,37 @@ public class VerificationService {
 	@Qualifier("verificationHandlerMap")
     private Map<String, AbstractVerificationHandler> verificationHandlerMap;
 
-
     /**
      * 生成验证信息
-     * @param name 业务名称
-     * @param payload 业务携带参数，如手机号 ，邮箱
+     *
+     * @param name          业务名称
+     * @param payload       业务携带参数，如手机号 ，邮箱
      * @param effectiveTime 验证信息有效期(秒)
      * @return
      */
-    public VerificationInfo generateVerificationInfo(String name, Map<String,Object> payload,  int effectiveTime){
-        AbstractVerificationHandler verificationHandler =  getVerificationHandler(name);
+    public VerificationInfo generateVerificationInfo(String name, Map<String, Object> payload, int effectiveTime) {
+        AbstractVerificationHandler verificationHandler = getVerificationHandler(name);
         return verificationHandler.generateVerificationInfo(payload, effectiveTime);
     }
 
     /**
      * 验证信息
-     * @param name 业务名称
+     *
+     * @param name             业务名称
      * @param verificationKey  验证key
      * @param verificationCode 验证码
      * @return
      */
-    public boolean verify(String name, String verificationKey, String verificationCode){
-        AbstractVerificationHandler verificationHandler =  getVerificationHandler(name);
+    public boolean verify(String name, String verificationKey, String verificationCode) {
+        AbstractVerificationHandler verificationHandler = getVerificationHandler(name);
         return verificationHandler.verify(verificationKey, verificationCode);
     }
 
     private AbstractVerificationHandler getVerificationHandler(String name) {
         AbstractVerificationHandler verificationHandler = verificationHandlerMap.get(name);
-        if(verificationHandler == null){
+        if (verificationHandler == null) {
             throw new RuntimeException(String.format("No found handler process %s type.", name));
         }
         return verificationHandler;
     }
-
 }
